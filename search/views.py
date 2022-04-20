fdf4c49f8b6e809c759fb2c98b898f7cf2c05dc6
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-04-20 03:28:59 +0200

    Add models and views

---
+++ search/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.db.models import Q
+from .models import Verses
+
+
+# Create your views here.
+
+def index(request):
+    verses = Verses.objects.all()
+    verses = {'verses': verses}
+    return render(request, 'search/search.html', verses)
+
+
+def search_by_word(request):
+    words = request.POST.get('ayah')
+    if words:
+        verses = Verses.objects.filter(
+            Q(verse__icontains=words) | Q(verseWithoutTashkeel__icontains=words)
+        )
+        print(verses)
+        verses = {'verses': verses}
+        print(verses)
+        return render(request, 'search/search.html', verses)
+    else:
+        return index(request)
+
+
+def get_surah(request, surah_id):
+    surah_pk = f"S{str(surah_id).zfill(3)}"
+    surah = Verses.objects.filter(
+        Q(verse_pk__icontains=surah_pk)
+    )
+    verses = {'verses': surah}
+    return render(request, 'search/search.html', verses)
+
+
+def api_docs(request):
+    return render(request, 'search/api.html')
