88f5f180b56b21ffbb29bfe8db55645cba2274ce
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-04-20 04:21:07 +0200

    Update config. files for Heroku

--- api/settings.py
+++ api/settings.py
@@ -30,9 +30,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'admin_interface',
-    'colorfield',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
