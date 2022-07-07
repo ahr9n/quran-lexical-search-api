0e6f54759e953bbfa6ec2e795c6e0a2fa6a8ed1a
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-07-07 14:39:46 +0200

    Clean up and fix bugs

--- run.sh
+++ run.sh
@@ -3,12 +3,6 @@
 # Install requirements
 pip install -r requirements.txt
 
-# Create new migrations based on the changes in models
-python3 manage.py makemigrations
-
-# Apply the migrations to the database
-python3 manage.py migrate
-
 # Create a superuser to be able to use Django Admin Interface
 python3 manage.py createsuperuser
 
