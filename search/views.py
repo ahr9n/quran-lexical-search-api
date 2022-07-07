e625e578b046afbe34cb9546f2b7662e566dcd5d
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2022-07-07 21:17:10 +0200

    Add semantic search views (connect apis and retrieve from database)

--- search/views.py
+++ search/views.py
@@ -1,7 +1,10 @@
+from unittest import result
 from django.shortcuts import render
 from django.db.models import Q
 from .models import Verses
-
+import requests  
+import json
+from urllib import parse
 
 # Create your views here.
 
@@ -13,13 +16,34 @@ def index(request):
 def search_by_word(request):
     words = request.POST.get('ayah')
     if words:
-        verses = Verses.objects.filter(
-            Q(verse__icontains=words) | Q(verseWithoutTashkeel__icontains=words)
-        )
-        print(verses)
-        verses = {'verses': verses}
-        print(verses)
-        return render(request, 'search/search.html', verses)
+        if 'lexical' in request.POST:
+            verses = Verses.objects.filter(
+                Q(verse__icontains=words) | Q(verseWithoutTashkeel__icontains=words)
+            )
+            search_result = 'lexical'
+            print(verses)
+        else: # 'semantic' is in request.POST
+            words = parse.quote(words)
+            
+            # I launched the lexical search on a different port
+            url = f"http://localhost:5000/similar-verse/{words}"
+            headers = {'content-type': 'application/json'}
+            results = requests.get(url, headers=headers)
+            search_result = 'semantic'
+            
+            # convert json to map to iterate over verses
+            results = json.loads(results)
+            results = results['results']
+
+            # get verses from database
+            verses = []
+            for score, verse_id, verse in results:
+                verse = Verses.objects.get(id=verse_id)
+                verses.append(verse)
+            
+        out = {'verses': verses, 'search_result': search_result}
+        # print(verses)
+        return render(request, 'search/search.html', out)
     else:
         return index(request)
 
